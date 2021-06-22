e5e39e5be91dd6145f86415c4f0e1e4e6ecd48bd
Nicolás Larena <nic.lar.universidad@gmail.com>
2021-06-22 02:51:14 -0300

    ASM DISTINTO

--- src/main/ast/NodoDistinto.java
+++ src/main/ast/NodoDistinto.java
@@ -1,7 +1,39 @@
 package main.ast;
 
+import main.assembler.recorrido.ConstantesASM;
+
 public class NodoDistinto extends NodoComparacionExpresiones {
+
+    private static int count = 0;
+    private final int distNumber;
+
     public NodoDistinto(NodoExpresion expresionIzquierda, NodoExpresion expresionDerecha) {
         super(expresionIzquierda, expresionDerecha, "<>");
+        count++;
+        distNumber = count;
+        this.setID("@_EQ" + count);
+    }
+
+    @Override
+    public String generarAssembler() {
+        ConstantesASM.data.append(this.getID()).append(" DW ?\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        if (! expresionIzquierda.soyHoja()) {
+            stringBuilder.append(expresionIzquierda.generarAssembler());
+        }
+        if (! expresionDerecha.soyHoja()) {
+            stringBuilder.append(expresionDerecha.generarAssembler());
+        }
+        String comparar = "MOV AX, " + expresionDerecha.getID()  + "\n" +
+                "MOV BX, " + expresionIzquierda.getID()  + "\n" +
+                "CMP AX, BX\n";
+        stringBuilder.append(comparar);
+        stringBuilder.append("JNE EQ_TRUE").append(distNumber).append("\n");
+        stringBuilder.append("MOV ").append(this.getID()).append(", 0").append("\n");
+        stringBuilder.append("JMP EQ_END").append(distNumber).append("\n");
+        stringBuilder.append("EQ_TRUE").append(distNumber).append(":").append("\n");
+        stringBuilder.append("MOV ").append(this.getID()).append(", 1").append("\n");
+        stringBuilder.append("EQ_END").append(distNumber).append(":").append("\n");
+        return stringBuilder.toString();
     }
 }
