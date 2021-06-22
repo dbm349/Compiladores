61965d3b816b69c75ff60073eab37ee146983b3a
ncavasin <ncavasin97@gmail.com>
2021-06-22 19:28:13 -0300

    Assembler para suma

--- src/main/ast/NodoSuma.java
+++ src/main/ast/NodoSuma.java
@@ -1,8 +1,30 @@
 package main.ast;
 
+import main.assembler.recorrido.ConstantesASM;
+
 public class NodoSuma extends NodoExpresionBinaria {
 
     public NodoSuma(NodoExpresion izquierda, NodoExpresion derecha) {
         super("+", izquierda, derecha);
+    }
+
+    public String generarAssembler() {
+
+        ConstantesASM.data.append(this.getID()).append(" DW, ?");
+
+        String result = "";
+        if (!izquierda.soyHoja()) {
+            result += izquierda.generarAssembler();
+        }
+        if (!derecha.soyHoja()) {
+            result += derecha.generarAssembler();
+        }
+        result += "\n;inicio suma;'\n";
+        result += "FLD " + derecha.getID() + "\n";
+        result += "FLD " + izquierda.getID() + "\n";
+        result += "FADD" + "\n";
+        result += "FSTP" + this.getID() + "\n";
+        result += "\n\n;fin suma;\n";
+        return result;
     }
 }
