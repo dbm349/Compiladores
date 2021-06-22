cc258505bb2bc0e9d69dc43f1e8dc51e25589a94
ncavasin <ncavasin97@gmail.com>
2021-06-22 20:30:34 -0300

    alternativa comentada

--- src/main/ast/NodoSalida.java
+++ src/main/ast/NodoSalida.java
@@ -20,5 +20,15 @@ public class NodoSalida extends NodoSentencia {
         String nombreVariableAssembler = "_" + constante_string.getValor().replace("\"", "").replace(".", "punto").replace(" ", "");
         return "displayString " + nombreVariableAssembler + "\n"
                 + "newLine 1 \n";
+/*        return "\n;ini muestra string\n" +
+        "mov dx, OFFSET " + nombreVariableAssembler + "\n" +
+        "mov ah, 9\n" +
+        "int 21h\n" +
+        "mov ah, 2\n" +
+        "mov dl, 13\n" +
+        "int 21h\n" +
+        "mov dl, 10\n" +
+        "int 21h\n" +
+        ";fin muestra string\n\n";*/
     }
 }
