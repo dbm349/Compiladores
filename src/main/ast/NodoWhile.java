32516b9b5ed0141821f92293db862ce774dbf6a8
Nicolás Larena <nic.lar.universidad@gmail.com>
2021-06-22 15:58:07 -0300

    ASM WHILE

--- src/main/ast/NodoWhile.java
+++ src/main/ast/NodoWhile.java
@@ -3,6 +3,9 @@ package main.ast;
 import java.util.List;
 
 public class NodoWhile extends NodoSentencia {
+    static int count = 0;
+    protected int ifCount;
+
     private final NodoCondicion condicion;
     //private final List<NodoSentencia> sentencias;
     protected final NodoBloque bloque;
@@ -11,6 +14,8 @@ public class NodoWhile extends NodoSentencia {
         super("WHILE");
         this.condicion = condicion;
         this.bloque = new NodoBloque(sentencias,"Bloque");
+        count++;
+        ifCount = count;
     }
 
     @Override
@@ -19,5 +24,18 @@ public class NodoWhile extends NodoSentencia {
         StringBuilder resultado = new StringBuilder(super.graficar(idPadre) +
                 condicion.graficar(miId) + bloque.graficar(miId));
         return resultado.toString();
+    }
+
+    @Override
+    public String generarAssembler() {
+        return  "INST_WHILE_BEGIN" + ifCount + ":"
+                + condicion.generarAssembler()
+                + "MOV AX, 1\n"
+                + "MOV BX, " + condicion.getID()  + "\n"
+                + "CMP AX, BX \n"
+                + "CMP " + condicion.getID() + ", 1\n"
+                + "JNE INST_WHILE_END" + ifCount + "\n"
+                + bloque.generarAssembler()
+                + "INST_WHILE_END" + ifCount + ":\n";
     }
 }
