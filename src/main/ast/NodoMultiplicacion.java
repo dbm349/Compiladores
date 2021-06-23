d97d94bcbb7673a486b7292d68af02c97d761deb
Nicolás Larena <nic.lar.universidad@gmail.com>
2021-06-22 21:31:27 -0300

    ASM MUL, ASM DIV

--- src/main/ast/NodoMultiplicacion.java
+++ src/main/ast/NodoMultiplicacion.java
@@ -15,7 +15,7 @@ public class NodoMultiplicacion extends NodoExpresionBinaria {
 
     public String generarAssembler() {
 
-        ConstantesASM.data.append(this.getID()).append(" DD, ?\n");
+        ConstantesASM.data.append(this.getID()).append(" DW, ?\n");
 
         String result = "";
         if (!izquierda.soyHoja()) {
@@ -26,11 +26,20 @@ public class NodoMultiplicacion extends NodoExpresionBinaria {
         }
 
         result += "\n;inicio multiplicacion*\n";
+        result += "\tXOR EAX, EAX\n";
+        result += "\tXOR EBX, EBX\n";
+        result += "\tMOV AX, " + derecha.getID() + "\n";
+        result += "\tMOV BX, " + izquierda.getID() + "\n";
+        result += "\tIMUL EBX"+ "\n";
+        result += "\tMOV " + this.getID() + ", AX\n";
+        result += ";fin multiplicacion*\n\n";
+
+        /*result += "\n;inicio multiplicacion*\n";
         result += "\tFLD " + derecha.getID() + "\n";
         result += "\tFLD " + izquierda.getID() + "\n";
         result += "\tFMUL "+ "\n";
         result += "\tFSTP " + this.getID() + "\n";
-        result += ";fin multiplicacion*\n\n";
+        result += ";fin multiplicacion*\n\n";*/
         return result;
     }
 }
